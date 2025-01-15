a1125bb3a70d6f3e6ad077f1f8e172814e4eb313
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2025-01-15 15:12:54 +0300

    Update titanic_analysis.py

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import sys
-import subprocess
-subprocess.check_call([sys.executable, "-m", "pip", "install", "seaborn"])
 import seaborn as sns
 import matplotlib.pyplot as plt
 
